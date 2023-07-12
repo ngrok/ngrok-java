7c07fa113846eff0d7560f5b0b41d65d70721af7
Carl Amko <carl@ngrok.com>
2023-07-12 11:41:38 -0600

    Fix docs after rebase

--- ngrok-java-native/src/main/java/com/ngrok/Runtime.java
+++ ngrok-java-native/src/main/java/com/ngrok/Runtime.java
@@ -14,14 +14,23 @@ import java.util.Locale;
  */
 class Runtime {
 
-    private static final Logger LOGGER = new Logger(); 
+    /**
+     * The singleton instance of {@link Runtime.Logger} used by the Runtime class.
+     */
+    private static final Logger LOGGER = new Logger();
 
+    /**
+     * Returns the {@link Runtime.Logger} used by the Runtime class.
+     *
+     * @return the singleton logger
+     */
     public static Logger getLogger() {
         return LOGGER;
     }
 
     /**
-     * Returns the name of the native library to load based on the current operating system.
+     * Returns the name of the native library to load based on the current operating
+     * system.
      *
      * @return the name of the native library to load
      * @throws RuntimeException if the operating system is unknown
@@ -42,7 +51,8 @@ class Runtime {
     /**
      * Loads the native library for the ngrok Java client.
      *
-     * @throws RuntimeException if an I/O error occurs or the native library fails to load
+     * @throws RuntimeException if an I/O error occurs or the native library fails
+     *                          to load
      */
     public static void load() {
         String filename = getLibname();
@@ -83,8 +93,8 @@ class Runtime {
                     .supportedFileAttributeViews()
                     .contains("posix");
         } catch (FileSystemNotFoundException
-                 | ProviderNotFoundException
-                 | SecurityException e) {
+                | ProviderNotFoundException
+                | SecurityException e) {
             return false;
         }
     }
@@ -99,16 +109,35 @@ class Runtime {
     /**
      * A class representing a logger for the runtime environment.
      */
+    /**
+     * A static class representing a logger for the Runtime class.
+     */
     static class Logger {
+        /**
+         * The logger used by the class.
+         */
         private static final org.slf4j.Logger logger = LoggerFactory.getLogger(Runtime.class);
+
+        /**
+         * The format used for logging messages.
+         */
         private static final String format = "[{}] {}";
 
-        private Logger() { }
+        /**
+         * Constructs a new logger.
+         */
+        private Logger() {
+        }
 
+        /**
+         * Returns the logging level of the logger.
+         *
+         * @return the logging level of the logger
+         */
         public String getLevel() {
             Level logLevel = Level.INFO;
 
-            Level[] levels = new Level[] {Level.ERROR, Level.WARN, Level.INFO, Level.DEBUG, Level.TRACE};
+            Level[] levels = new Level[] { Level.ERROR, Level.WARN, Level.INFO, Level.DEBUG, Level.TRACE };
             for (Level level : levels) {
                 if (logger.isEnabledForLevel(level)) {
                     logLevel = level;
@@ -118,8 +147,15 @@ class Runtime {
             return logLevel.toString();
         }
 
+        /**
+         * Logs a message with the specified level and target.
+         *
+         * @param level   the level of the message
+         * @param target  the target of the message
+         * @param message the message to log
+         */
         public void log(String level, String target, String message) {
-            Level lvl = Level.valueOf(level.toUpperCase()); 
+            Level lvl = Level.valueOf(level.toUpperCase());
             logger.atLevel(lvl).log(format, target, message);
         }
     }
