3b9c45600918b5aca12352a4dda7e1e165fde748
Andrew Lawrence <alawrence@ngrok.com>
2024-02-29 11:01:40 -0500

    update to jetty 12

--- ngrok-jetty/src/main/java/com/ngrok/jetty/NgrokEndpoint.java
+++ ngrok-jetty/src/main/java/com/ngrok/jetty/NgrokEndpoint.java
@@ -1,13 +1,14 @@
 package com.ngrok.jetty;
 
-import com.ngrok.Connection;
+import java.io.IOException;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.util.concurrent.TimeUnit;
 
 import org.eclipse.jetty.io.AbstractEndPoint;
 import org.eclipse.jetty.util.thread.Scheduler;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.concurrent.TimeUnit;
+import com.ngrok.Connection;
 
 /**
  * A class representing an endpoint for ngrok connection.
@@ -29,7 +30,8 @@ public class NgrokEndpoint extends AbstractEndPoint {
     }
 
     /**
-     * Throws an {@link UnsupportedOperationException}, as incomplete flush is not supported
+     * Throws an {@link UnsupportedOperationException}, as incomplete flush is not
+     * supported
      * by ngrok endpoints.
      *
      * @throws UnsupportedOperationException if the method is called
@@ -80,7 +82,8 @@ public class NgrokEndpoint extends AbstractEndPoint {
     }
 
     /**
-     * Throws an {@link UnsupportedOperationException}, as the transport used by ngrok
+     * Throws an {@link UnsupportedOperationException}, as the transport used by
+     * ngrok
      * endpoints is not supported.
      *
      * @throws UnsupportedOperationException if the method is called
@@ -88,5 +91,15 @@ public class NgrokEndpoint extends AbstractEndPoint {
     @Override
     public Object getTransport() {
         throw new UnsupportedOperationException("ohnoe");
+    }
+
+    @Override
+    public SocketAddress getLocalSocketAddress() {
+        return null;
+    }
+
+    @Override
+    public SocketAddress getRemoteSocketAddress() {
+        return conn.inetAddress();
     }
 }
